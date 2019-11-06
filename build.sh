92a315b9cbe6ee952860ff3b7319f1df0098be00
eraming <40274451+eraming@users.noreply.github.com>
2019-11-05 18:34:57 -0800

    Delete build.sh

--- build.sh
+++
@@ -1,5 +0,0 @@
-cd  docs/
-cat ../templates/top.html ../content/index.html ../templates/bottom.html > index.html
-cat ../templates/top.html ../content/gallery.html ../templates/bottom.html > gallery.html
-cat ../templates/top.html ../content/projects.html ../templates/bottom.html > projects.html
-cat ../templates/top.html ../content/contact.html ../templates/bottom.html > contact.html
