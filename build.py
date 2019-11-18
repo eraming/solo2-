fc53e4bad793b3803e2b97ab86b09cd0e42427c3
Era Ming <era.y.ming@gmail.com>
2019-11-18 15:36:26 -0800

    adding the utils and manage

--- build.py
+++ build.py
@@ -1,32 +1,39 @@
 
+import jinja2
 
 pages = [
 {
     "filename": "content/index.html",
     "output": "docs/index.html",
-    "title": "Home Page",
+    "title": "home page",
+    "header": "era ming",
 },
 {
     "filename": "content/gallery.html",
     "output": "docs/gallery.html",
-    "title": "Film Photography",
+    "title": "film photography",
+    "header": "film photography",
 },
 {
     "filename": "content/projects.html",
     "output": "docs/projects.html",
-    "title": "Projects",
+    "title": "projects",
+    "header": "past and current projects",
 },
 {
     "filename": "content/contact.html",
     "output": "docs/contact.html",
     "title": "Contact Me",
+    "header": "how to reach me",
 }
 ]
 
+
 def compile(content, page):
     template = open("./templates/base.html").read()
-    finished_content = template.replace("{{content}}", content).replace("{{title}}", page['title'])
-    # returning the string replacement for content and title
+    # finished_content = template.replace("{{content}}", content).replace("{{title}}", page['title']).replace("{{header}}", page['header'])
+    jinja_template = jinja2.Template(template)
+    finished_content = template.render(title=page['title'], content=content)
     return finished_content
 
 def write(page, finished_content):
---
+++ utils.py
@@ -0,0 +1,76 @@
+
+import glob
+import os
+import jinja2
+
+all_html_files = glob.glob("./content/*.html")
+
+pages = []
+
+def compile_pages():
+    for html_file in all_html_files:
+        file_name = os.path.basename(html_file)
+        # print(file_name)
+        name_no_extension, extension = os.path.splitext(file_name)
+        # print(name_no_extension)
+        pages.append({
+            'filename': ('./content/' + file_name),
+            'output': ('./docs/' + file_name),
+            'title': '{{ title }}',
+            'header': '{{ header }}',
+        })
+
+def open_base():
+    base_template = open("./templates/base.html").read()
+    return base_template
+
+def open_content(page):
+    content = open(page['filename']).read()
+    return content
+
+def compile(page, base_template, content):
+    jinja_template = jinja2.Template(base_template)
+    base_with_content = jinja_template.render(pages=pages, title=page['title'], content=content)
+    return base_with_content
+
+def write(page, base_with_content):
+    final_page = open(page['output'], "w+").write(base_with_content)
+    return final_page
+
+
+def main():
+    compile_pages()
+
+    for page in pages:
+        base_template = open_base()
+        content = open_content(page)
+        base_with_content = compile(page, base_template, content)
+        write(page, base_with_content)
+
+
+
+
+
+# print('------------- Example 3')
+
+# template = Template('''
+# Using if and for loops combined:
+#
+# {% for link in links %}
+#     {% if link == selected_link %} >> {% endif %} {{ link }}
+# {% endfor %}
+# ''')
+#
+# list_of_links = [
+#     'google.com',
+#     'microsoft.com',
+#     'facebook.com',
+# ]
+#
+# selected = 'microsoft.com'
+#
+# results = template.render({
+#     'links': list_of_links,
+#     'selected_link': selected,
+# })
+# print(results)
